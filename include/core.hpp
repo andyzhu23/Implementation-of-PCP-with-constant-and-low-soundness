765b7f1d6b822e6fe6775b8d7331d65f3bf624d5
andyzhu23 <andy.zheyuan.zhu@gmail.com>
2025-10-16 10:21:39 +0100

    [milestone 1] added a comment

--- include/core.hpp
+++ include/core.hpp
@@ -6,6 +6,7 @@
 
 namespace core {
 
+// Powering operation on a BitPCP to produce a PoweringPCP from Dinur's PCP proof
 pcp::PoweringPCP powering_operation(const pcp::BitPCP &pcp, int radius);
 
 }
