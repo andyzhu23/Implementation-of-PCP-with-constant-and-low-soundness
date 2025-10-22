410c9593c8e86608de24b63c86fb315dde69ea78
andyzhu23 <andy.zheyuan.zhu@gmail.com>
2025-10-22 21:23:03 +0100

    fixed cyclic dependencies

---
+++ include/pcp/PCP.hpp
@@ -0,0 +1,17 @@
+#ifndef PCP_HPP
+#define PCP_HPP
+
+#include <vector>
+
+namespace pcp {
+
+// type used to represent variable index
+using Variable = size_t;
+// type used to represent constraint index
+using Index = size_t;
+// type used to represent domain of values in simplePCP
+using SimpleDomain = int;
+
+}
+
+#endif
