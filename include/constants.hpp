43db2d2b9bf45f185d9083fe69b82e0764938bcd
andyzhu23 <andy.zheyuan.zhu@gmail.com>
2025-12-19 20:51:18 -0800

    added flag to toggle off heavy tests on github push

--- include/constants.hpp
+++ include/constants.hpp
@@ -9,7 +9,6 @@ const int LINEARITY_COEFFICIENT = 100;
 const int POWERING_RADIUS = 4;
 const int EXPANDING_COEFFICIENT = 1;
 const int DEGREE = 4;
-const int SCALING_FACTOR = 1;
 const unsigned int SAFE_THREAD_NUMBER = 4;
 const pcp::Variable PCPVARIABLE_ONE = 1;
 
