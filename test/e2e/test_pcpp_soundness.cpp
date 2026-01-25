cc89bffb5330dc6794fc8323d85bcf48894848cd
andyzhu23 <andy.zheyuan.zhu@gmail.com>
2026-01-25 20:36:22 +0000

    modified test_pcpp_soundness

--- test/e2e/test_pcpp_soundness.cpp
+++ test/e2e/test_pcpp_soundness.cpp
@@ -10,33 +10,7 @@
 #include "pcpp/Tester.hpp"
 
 std::vector<std::function<void()>> test_cases = {
-    // // Test 1: Simple non-three-colorable graph
-    // []() -> void {
-    //     three_color::ThreeColor input({three_color::Color::RED, three_color::Color::RED, three_color::Color::RED, three_color::Color::RED}, {{0, 1}, {0, 2}, {0, 3}, {1, 2}, {2, 3}});
-    //     pcp::BitPCP bitpcp = input.to_BitPCP();
-    //     pcp::BitPCP amplified_pcp = core::gap_amplification(bitpcp);
-    //     double original_soundness = analyzer::approximate_soundness(bitpcp);
-    //     double amplified_soundness = analyzer::approximate_soundness(amplified_pcp);
-    //     std::cout << "Approximated original gap: " << 1 - original_soundness << std::endl;
-        
-    //     std::cout << "Approximated amplified gap: " << 1 - amplified_soundness << std::endl;
-    // },
-    // Test 1: Simple non-satisfiable CSP
-    // []() -> void {
-    //     pcp::BitPCP bitpcp(2);
-    //     bitpcp.add_constraint(1, 0, constraint::BitConstraint::EQUAL); 
-    //     bitpcp.add_constraint(1, 0, constraint::BitConstraint::NOTEQUAL); // make it unsatisfiable
-
-    //     pcpp::Tester tester(bitpcp);
-    //     pcp::BitPCP amplified_pcp = tester.buildBitPCP();
-
-    //     double original_soundness = analyzer::approximate_soundness(bitpcp);
-    //     double amplified_soundness = analyzer::approximate_soundness(amplified_pcp);
-    //     std::cout << "Approximated original gap: " << 1 - original_soundness << std::endl;
-        
-    //     std::cout << "Approximated amplified gap: " << 1 - amplified_soundness << std::endl;
-    // },
-    // Test 2: Simple non-satisfiable CSP
+    // Simple non-satisfiable CSP
     []() -> void {
         pcp::BitPCP bitpcp(10);
         for (pcp::Variable i = 0; i < 10; ++i) {
@@ -47,14 +21,14 @@ std::vector<std::function<void()>> test_cases = {
         }
         bitpcp.add_constraint(9, 0, constraint::BitConstraint::EQUAL); // make it satisfiable
         pcpp::Tester tc(bitpcp);
-        pcp::BitPCP amplified_pcp = tc.buildBitPCP();
+        pcp::BitPCP pcpp = tc.buildBitPCP();
         double original_soundness = analyzer::approximate_soundness(bitpcp);
-        double amplified_soundness = analyzer::approximate_soundness(amplified_pcp);
+        double pcpp_soundness = analyzer::approximate_soundness(pcpp);
         std::cout << "Approximated original gap: " << 1 - original_soundness << std::endl;
-        
-        std::cout << "Approximated amplified gap: " << 1 - amplified_soundness << std::endl;
+        std::cout << "Approximated pcpp gap: " << 1 - pcpp_soundness << std::endl;
+        assert(1 - pcpp_soundness == 0);
     },
-    // Test 2: Simple non-satisfiable CSP
+    // Simple non-satisfiable CSP
     []() -> void {
         pcp::BitPCP bitpcp(10);
         for (pcp::Variable i = 0; i < 10; ++i) {
@@ -66,13 +40,13 @@ std::vector<std::function<void()>> test_cases = {
         bitpcp.add_constraint(9, 0, constraint::BitConstraint::NOTEQUAL); // make it unsatisfiable
 
         pcpp::Tester tc(bitpcp);
-        pcp::BitPCP amplified_pcp = tc.buildBitPCP();
+        pcp::BitPCP pcpp = tc.buildBitPCP();
         double original_soundness = analyzer::approximate_soundness(bitpcp);
-        double amplified_soundness = analyzer::approximate_soundness(amplified_pcp);
+        double pcpp_soundness = analyzer::approximate_soundness(pcpp);
         std::cout << "Approximated original gap: " << 1 - original_soundness << std::endl;
-        
-        std::cout << "Approximated amplified gap: " << 1 - amplified_soundness << std::endl;
-    }
+        std::cout << "Approximated pcpp gap: " << 1 - pcpp_soundness << std::endl;
+        assert(pcpp_soundness > 0);
+    },
 };
 
 int main() {
